7cbccb4f820633472383fcba0373241b0deb41d2
AquaL <vlad-maltsev-2024@mail.ru>
2023-05-24 17:08:41 +0700

    created NotesApi.js NotesUI.js and new fich

---
+++ js/NotesUI.js
@@ -0,0 +1,131 @@
+export default class NotesUI{
+
+  constructor(notesData, root){
+
+    this.notesData = notesData;
+    this.activeNoteId = this.notesData.getLastId() || 1;
+    this.activeNote = {};
+    this.root = root;
+
+    this.notesPreview = this.root.querySelector('#notes-preview');
+    this.addNoteBtn = this.root.querySelector('#add-note');
+    this.noteTitle = this.root.querySelector('#note-title');
+    this.noteContent = this.root.querySelector('#note-content');
+    this.searchInput = this.root.querySelector('#search-input');
+
+
+    this.addNoteBtn.addEventListener('click', ()=>{
+      this.createNote();
+    });
+
+    this.noteTitle.addEventListener('input', (e)=>{
+      e.preventDefault();
+      console.log(e);
+      if(e.inputType == 'insertParagraph'){
+        this.noteContent.focus();
+      }
+      this.updatedNote();
+    });
+
+    this.noteContent.addEventListener('input', ()=>{
+      this.updatedNote();
+    });
+
+    this.searchInput.addEventListener('input', ()=>{
+      this.initUI();
+    })
+
+    this.initUI();
+
+  }
+
+  initUI(){
+    this.renderListNotes();
+    this.renderNote();
+  }
+
+  initListNotesItem(id,title,body,updated){
+    let active = '';
+
+    if (id == this.activeNoteId){
+      active = 'note-preview_active';
+    }
+
+    const note =
+      `<div class='note-preview ${active}' data-note-id=${id}>
+        <h3 class='note-preview__title'>${title}</h3>
+        <p class='note-preview__text'>${body}</p>
+        <p class='note-preview__date'>${updated}</p>
+      </div>`;
+
+    return note;
+
+  }
+
+  initListNotes(){
+    let notesList = '';
+    const searchStr = this.searchInput.value;
+
+    let notes = this.notesData.getNotes(searchStr);
+
+    if(notes.length == 0 && searchStr == ''){
+      this.notesData.createdNote();
+      notes = this.notesData.getNotes();
+    }
+
+    notes.forEach(note => {
+      notesList += this.initListNotesItem(note.id,note.title,note.body, note.updated);
+      if (note.id == this.activeNoteId){
+        this.activeNote = note;
+      }
+
+    });
+
+    return notesList;
+  }
+
+  renderListNotes(){
+    this.notesPreview.innerHTML = this.initListNotes();
+
+    const notesList = this.root.querySelectorAll('.note-preview');
+    notesList.forEach(noteBtn => {
+      noteBtn.addEventListener('click', ()=>{
+        this.noteBtnClick(noteBtn);
+      })
+    });
+
+  }
+
+  noteBtnClick(button){
+    this.activeNoteId = button.dataset.noteId;
+    this.renderListNotes();
+    this.renderNote();
+  }
+
+  renderNote(){
+    this.noteTitle.innerHTML = this.activeNote.title;
+    this.noteContent.innerHTML = this.activeNote.body;
+  }
+
+  createNote(){
+    const createdNoteId = this.notesData.createdNote();
+    this.activeNoteId = createdNoteId;
+
+    this.renderListNotes();
+    this.renderNote();
+
+  }
+
+  updatedNote(){
+    const newNote = {
+      id: this.activeNote.id,
+      title: this.noteTitle.innerHTML,
+      body: this.noteContent.innerHTML,
+      updated: new Date()
+    }
+
+    this.notesData.updateNote(newNote);
+    this.renderListNotes();
+  }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-import NotesAPI from './NotesAPI.js'
+import NotesAPI from "./NotesAPI.js"
+import NotesUI from "./NotesUI.js"
+
 
 window.addEventListener("load", main);
 
 function main() {
+
 
   const themeBtn = document.getElementById("theme-btn");
   const page = document.getElementById("page");
@@ -11,25 +14,25 @@ function main() {
   const subMenuBtn = document.getElementById("main-menu-additional-btn");
   const subMenuBtnIcon = document.getElementById("main-menu-additional-btn-icon");
   const subMenu = document.getElementById("main-menu-additional");
+  const notesPreview = document.getElementById("notes-preview");
 
+  const notesData = new NotesAPI();
+  const app = new NotesUI(notesData, page);
 
-  themeBtn.addEventListener("click", ()=>{
+  themeBtn.addEventListener("click", ()=> {
     page.classList.toggle('page_dark');
   });
 
-  menuBtn.addEventListener("click", ()=>{
+  menuBtn.addEventListener("click", ()=> {
     sidebar.classList.toggle('sidebar-wrapper_hidden');
     menuBtn.classList.toggle('button_active');
   });
 
-  subMenuBtn.addEventListener("click", ()=>{
+  subMenuBtn.addEventListener("click", ()=> {
     subMenu.classList.toggle("main-menu-additional_visible");
     subMenuBtn.classList.toggle("button_active");
     subMenuBtnIcon.classList.toggle("main-menu-additional-btn-icon_active");
   });
 
-
-
-  const notesData = new NotesAPI();
-  console.log(notesData.notes)
 }
+
